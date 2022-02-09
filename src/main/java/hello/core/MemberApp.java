e6a8a1c1d9648134959394268c1b6d1e021384f9
Joung Min Ju <alswn9938@gmail.com>
2022-02-09 19:27:36 +0900

    [22.02.09] 섹션4 2강

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,24 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // 코드로 테스트해보기 위한 클래스
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
         // 멤버 서비스  인터페이스 받고
         // new memberserviceimpl 객체 생성하면서 memory member repository 받음.
-        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = appConfig.memberService();
+
+        // ApplicationContext == 스프링 컨테이너(bean관리)
+        // appconfig의 환경설정 정보를 가지고 스프링이 컨테이너에 객체 생성해서 다 관리해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // (이름, 타입)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 
 //        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
