85ab8ff305f5df98cf2a52b2f39070e22a274d43
MINJU <alswn9938@gmail.com>
2022-02-21 18:31:21 +0900

    [22.02.20] 섹션9 끝

--- src/main/java/hello/core/logdemo/LogDemoService.java
+++ src/main/java/hello/core/logdemo/LogDemoService.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 //이 어노테이션은 초기화 되지않은 final 필드나, @NonNull 이 붙은 필드에 대해 생성자를 생성해 줍니다. 주로 의존성 주입(Dependency Injection) 편의성을 위해서 사용되곤 합니다.
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String id) {
+//        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import hello.core.logdemo.LogDemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,13 +16,18 @@ public class LogDemoController {
     private final LogDemoService logDemoService;
     // my logger를 내놓으려고 하는데, 얘가 request  scope임
     // 근데 request가 없다. 따라서 오류가 발생한다.
-    private final MyLogger myLogger;
 
+    //Myloger 찾을 수 있는 애가 됨.
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    private MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         // 고객이 어떤 url로 요청했는지 알아낼 수 있다.
+
+//        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
         myLogger.log("Controller test");
